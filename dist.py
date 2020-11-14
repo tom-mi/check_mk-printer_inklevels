c0f83080f0774f1a9e65e33d07ab4f7d63fe3e61
Thomas Reifenberger <tom-mi@users.noreply.github.com>
2020-11-14 15:38:48 +0100

    Cleanup
    - fix url
    - use non-deprecated command in release action

--- dist.py
+++ dist.py
@@ -6,7 +6,7 @@ import versioneer
 dist({
     'author': 'Thomas Reifenberger',
     'description': 'Monitor ink levels of printers via libinklevel',
-    'download_url': 'https://github.com/tom-mi/check_mk-printer_inklevel',
+    'download_url': 'https://github.com/tom-mi/check_mk-printer_inklevels',
     'name': 'printer_inklevels',
     'title': 'Printer Inklevels',
     'version': versioneer.get_version(),
